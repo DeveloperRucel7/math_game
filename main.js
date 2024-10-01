af4b01a4991e0507f968420b644b89ae3698fd60
DeveloperRucel7 <cybercorps237@gmail.com>
2024-10-02 00:53:53 +0200

    add a hidden value with position

--- main.js
+++ main.js
@@ -13,10 +13,11 @@ gameArea.append(btn);
 const opts = ['*','/','+','-']
 
 const game = {
-    max_value:10,
+    max_value:30,
     questions:10,
-    oper:[2,3],
-    current_question:0
+    Ovals:[0,1,2,3],
+    current_question:0,
+    hidden_value:3
 };
 
 btn.addEventListener('click', start_game)
@@ -39,9 +40,34 @@ function build_question(){
         let vals =[];
         vals[0]= Math.floor(Math.random()*(game.max_value+1));
         vals[1]= Math.floor(Math.random()*(game.max_value+1));
-        vals[2]= 
-        vals[3]= eval(vals[0]+ vals[2] + vals[1]);
-        output.innerHTML = `${vals[0]} ${vals[2]} ${vals[1]} = ${vals[3]}`
+        // randomize a postive or a negative return if we have anything less than 0.5
+        game.Ovals.sort(()=>{return 0.5 -Math.random()});
+
+        if(game.Ovals[0] ==1){
+            let temp = vals[0]*vals[1];
+            // replace the value of the temp in front of the vals array
+            vals.unshift(temp);
+            console.log(temp);
+        }
+        else{
+            vals[2]= eval(vals[0]+ opts[game.Ovals[0]] + vals[1]);
+        }
+        vals[3]= opts[game.Ovals[0]];
+        
+        console.log(vals)
+         let hidden_value 
+        //select the value where support to be the hidden one
+        if(game.hidden_value !=3){
+            hidden_value = game.hidden_value
+        }else{
+            // hidde a value  and show the others
+            hidden_value = Math.floor(Math.random()*3);
+        }
+       
+
+        vals[hidden_value]= '_';
+
+        output.innerHTML = `${vals[0]} ${vals[3]} ${vals[1]} = ${vals[2]}`
     }
    
 }
